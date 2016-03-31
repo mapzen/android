80f2557725e408da570c603c1fb68ef60ea1ffb3
Chuck Greb <charles.greb@gmail.com>
2016-03-31 09:28:19 -0400

    Replaces "app" module with "sample" module

---
+++ sample/src/main/java/com/mapzen/android/sample/SampleMapzenActivityFragment.java
@@ -0,0 +1,22 @@
+package com.mapzen.android.sample;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class SampleMapzenActivityFragment extends Fragment {
+
+    public SampleMapzenActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_sample_mapzen, container, false);
+    }
+}
