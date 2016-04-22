d3763296168757f9d9332b108a93de88f1a6a618
Chuck Greb <charles.greb@gmail.com>
2016-04-22 00:00:02 -0400

    Adds spinner for style selection

--- sample/src/main/java/com/mapzen/android/sample/SwitchStyleActivity.java
+++ sample/src/main/java/com/mapzen/android/sample/SwitchStyleActivity.java
@@ -7,54 +7,36 @@ import com.mapzen.android.model.MapStyle;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * Demonstrates switching the map's style.
  */
-public class SwitchStyleActivity extends AppCompatActivity {
+public class SwitchStyleActivity extends AppCompatActivity implements
+        AdapterView.OnItemSelectedListener {
 
     private MapFragment mapFragment;
     private MapzenMap mapzenMap;
-
-    private Button bubbleBtn;
-    private Button cinnabarBtn;
-    private Button refillBtn;
-    private Button outdoorBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_switch_style);
 
-        bubbleBtn = (Button) findViewById(R.id.btn_bubble);
-        bubbleBtn.setOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View v) {
-                changeMapStyle(MapStyle.BUBBLE_WRAP);
-            }
-        });
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        cinnabarBtn = (Button) findViewById(R.id.btn_cinnabar);
-        cinnabarBtn.setOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View v) {
-                changeMapStyle(MapStyle.CINNABAR);
-            }
-        });
-
-        refillBtn = (Button) findViewById(R.id.btn_refill);
-        refillBtn.setOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View v) {
-                changeMapStyle(MapStyle.REFILL);
-            }
-        });
-
-        outdoorBtn = (Button) findViewById(R.id.btn_outdoor);
-        outdoorBtn.setOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View v) {
-                changeMapStyle(MapStyle.OUTDOOR);
-            }
-        });
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.style_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
 
         mapFragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
         mapFragment.getMapAsync(MapStyle.BUBBLE_WRAP, new OnMapReadyCallback() {
@@ -65,6 +47,32 @@ public class SwitchStyleActivity extends AppCompatActivity {
     }
 
     private void changeMapStyle(MapStyle style) {
-        mapzenMap.setStyle(style);
+        if (mapzenMap != null) {
+            mapzenMap.setStyle(style);
+        }
+    }
+
+    @Override public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch (position) {
+            case 0:
+                changeMapStyle(MapStyle.BUBBLE_WRAP);
+                break;
+            case 1:
+                changeMapStyle(MapStyle.CINNABAR);
+                break;
+            case 2:
+                changeMapStyle(MapStyle.REFILL);
+                break;
+            case 3:
+                changeMapStyle(MapStyle.OUTDOOR);
+                break;
+            default:
+                changeMapStyle(MapStyle.BUBBLE_WRAP);
+                break;
+        }
+    }
+
+    @Override public void onNothingSelected(AdapterView<?> parent) {
+        // Do nothing.
     }
 }
