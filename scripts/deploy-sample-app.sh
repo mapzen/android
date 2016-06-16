1fa08b4e64be2ff4f322e284d3b678450c377c68
Chuck Greb <charles.greb@gmail.com>
2016-06-16 15:44:03 -0400

    Fix copy/paste gradle command assembleDevDebug -> assembleDebug

--- scripts/deploy-sample-app.sh
+++ scripts/deploy-sample-app.sh
@@ -2,6 +2,6 @@
 #
 # Builds sample app and uploads APK to s3://android.mapzen.com/sample-snapshots/.
 
-./gradlew assembleDevDebug -PvectorTilesKey=$VECTOR_TILES_KEY -PturnByTurnKey=$TURN_BY_TURN_KEY
+./gradlew assembleDebug -PvectorTilesKey=$VECTOR_TILES_KEY -PturnByTurnKey=$TURN_BY_TURN_KEY
 s3cmd put sample/build/outputs/apk/sample-debug.apk s3://android.mapzen.com/mapzen-sample-latest.apk
 s3cmd put sample/build/outputs/apk/sample-debug.apk s3://android.mapzen.com/sample-snapshots/mapzen-sample-$CIRCLE_BUILD_NUM.apk
