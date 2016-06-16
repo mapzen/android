8c6c897d84ecacbe7ccd4f2a258d9007f532807c
Chuck Greb <charles.greb@gmail.com>
2016-06-16 15:37:55 -0400

    Adds sample app deploy script

---
+++ scripts/deploy-sample-app.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#
+# Builds sample app and uploads APK to s3://android.mapzen.com/sample-snapshots/.
+
+./gradlew assembleDevDebug -PvectorTilesKey=$VECTOR_TILES_KEY -PturnByTurnKey=$TURN_BY_TURN_KEY
+s3cmd put sample/build/outputs/apk/sample-debug.apk s3://android.mapzen.com/mapzen-sample-latest.apk
+s3cmd put sample/build/outputs/apk/sample-debug.apk s3://android.mapzen.com/sample-snapshots/mapzen-sample-$CIRCLE_BUILD_NUM.apk
