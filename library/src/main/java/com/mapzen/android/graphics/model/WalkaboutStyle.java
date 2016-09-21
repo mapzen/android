b4c39880ab85d72eb5d9363da126e61877e97e1f
Chuck Greb <ecgreb@users.noreply.github.com>
2016-09-21 13:13:28 -0400

    Adds Walkabout style (#176)

---
+++ library/src/main/java/com/mapzen/android/graphics/model/WalkaboutStyle.java
@@ -0,0 +1,14 @@
+package com.mapzen.android.graphics.model;
+
+/**
+ * Walkabout outdoor map style.
+ */
+public class WalkaboutStyle extends MapStyle {
+
+  /**
+   * Creates a new instance.
+   */
+  public WalkaboutStyle() {
+    super("styles/walkabout-style-more-labels/walkabout-style-more-labels.yaml");
+  }
+}
