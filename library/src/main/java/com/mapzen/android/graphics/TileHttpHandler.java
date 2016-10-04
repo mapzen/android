e8087534bbb1859d86ad87d463dd70980a7c12a4
Sarah Lensing <sarah@mapzen.com>
2016-10-04 15:56:51 -0600

    Add tile request time logging

--- library/src/main/java/com/mapzen/android/graphics/TileHttpHandler.java
+++ library/src/main/java/com/mapzen/android/graphics/TileHttpHandler.java
@@ -4,14 +4,24 @@ import com.mapzen.tangram.HttpHandler;
 
 import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.HashMap;
 
 /**
  * A handler responsible for appending an API key to vector tile requests.
  */
 public class TileHttpHandler extends HttpHandler {
+  private static final String TAG = TileHttpHandler.class.getSimpleName();
+  private static final boolean DEBUG_REQUESTS = false;
+
   static final String PARAM_API_KEY = "?api_key=";
 
   private String apiKey;
+  private HashMap<String, Long> urlToMillis = new HashMap<>();
 
   /**
    * Creates a new HTTP handler.
@@ -34,8 +44,34 @@ public class TileHttpHandler extends HttpHandler {
   }
 
   @Override public boolean onRequest(String url, Callback cb) {
+    final Callback fnlCb = cb;
     final String urlWithKey = url + PARAM_API_KEY + apiKey;
-    return super.onRequest(urlWithKey, cb);
+    Callback internalCallback = new Callback() {
+      @Override public void onFailure(Request request, IOException e) {
+        String requestUrl = request.urlString();
+        urlToMillis.remove(requestUrl);
+        fnlCb.onFailure(request, e);
+      }
+
+      @Override public void onResponse(Response response) throws IOException {
+        String requestUrl = response.request().urlString();
+        if (DEBUG_REQUESTS) {
+          Long endMillis = System.currentTimeMillis();
+          Long startMillis = urlToMillis.get(requestUrl);
+          Long elapsedMillis = endMillis - startMillis;
+          boolean fromCache = response.cacheResponse() != null;
+          if (fromCache) {
+            Log.d(TAG, "loaded from cache [" + elapsedMillis + "] millis");
+          } else {
+            Log.d(TAG, "loaded from network [" + elapsedMillis + "] millis");
+          }
+        }
+        urlToMillis.remove(requestUrl);
+        fnlCb.onResponse(response);
+      }
+    };
+    urlToMillis.put(urlWithKey, System.currentTimeMillis());
+    return super.onRequest(urlWithKey, internalCallback);
   }
 
   @Override public void onCancel(String url) {
