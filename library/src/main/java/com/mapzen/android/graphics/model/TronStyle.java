9f08ff7601689ffd18c9492f15ba2cda9ac2bb66
Chuck Greb <charles.greb@gmail.com>
2016-10-14 18:39:01 -0400

    Adds Tron style

---
+++ library/src/main/java/com/mapzen/android/graphics/model/TronStyle.java
@@ -0,0 +1,14 @@
+package com.mapzen.android.graphics.model;
+
+/**
+ * Tron 2.0 map style.
+ */
+public class TronStyle extends MapStyle {
+
+  /**
+   * Creates a new instance.
+   */
+  public TronStyle() {
+    super("styles/tron-style/tron-style.yaml");
+  }
+}
