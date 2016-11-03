93a22c9cab4b64434a30cc8596da567828dacc64
Chuck Greb <charles.greb@gmail.com>
2016-11-03 13:53:27 -0400

    Removes zinc from custom stylesheet demo
    Also removes spinner from custom style demo since tron (original) is now only example of a custom stylesheet.
    
    Closes #220

--- sample/src/main/java/com/mapzen/android/sample/CustomStylesheetActivity.java
+++ sample/src/main/java/com/mapzen/android/sample/CustomStylesheetActivity.java
@@ -7,73 +7,29 @@ import com.mapzen.android.graphics.model.MapStyle;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 
 /**
  * Demonstrates how to set a custom stylesheet.
  */
-public class CustomStylesheetActivity extends AppCompatActivity
-    implements AdapterView.OnItemSelectedListener {
+public class CustomStylesheetActivity extends AppCompatActivity {
 
-  MapzenMap map;
-
-  MapStyle zincStyle = new MapStyle("styles/zinc/zinc.yaml");
-  MapStyle tronStyle = new MapStyle("styles/tron/tron.yaml");
+  public static final MapStyle tronStyle = new MapStyle("styles/tron/tron.yaml");
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_spinner);
-
-    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-    setSupportActionBar(toolbar);
-    getSupportActionBar().setDisplayShowTitleEnabled(false);
-
-    Spinner spinner = (Spinner) findViewById(R.id.spinner);
-    ArrayAdapter<CharSequence> adapter =
-        ArrayAdapter.createFromResource(this, R.array.custom_style_array,
-            R.layout.simple_spinner_item);
-    adapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item);
-    spinner.setAdapter(adapter);
-    spinner.setOnItemSelectedListener(this);
+    setContentView(R.layout.activity_sample_mapzen);
 
     final MapFragment mapFragment =
         (MapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
 
     final Bundle state = savedInstanceState;
-    mapFragment.getMapAsync(zincStyle, new OnMapReadyCallback() {
+    mapFragment.getMapAsync(tronStyle, new OnMapReadyCallback() {
       @Override public void onMapReady(MapzenMap map) {
-        CustomStylesheetActivity.this.map = map;
         if (state == null) {
-          map.setStyle(zincStyle);
+          map.setStyle(tronStyle);
         }
         map.setPersistMapState(true);
       }
     });
   }
-
-  @Override public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-    if (map == null) {
-      return;
-    }
-    switch (position) {
-      case 0:
-        map.setStyle(zincStyle);
-        break;
-      case 1:
-        map.setStyle(tronStyle);
-        break;
-      default:
-        map.setStyle(zincStyle);
-        break;
-    }
-  }
-
-  @Override public void onNothingSelected(AdapterView<?> parent) {
-
-  }
-
 }
