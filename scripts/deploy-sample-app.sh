49d9574fd5a0e66363f0fdb6e4447472689934d3
Sarah Lensing <sarah@mapzen.com>
2016-11-30 15:24:45 -0700

    Update sample app deploy scripts

--- scripts/deploy-sample-app.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#
-# Builds sample app and uploads APK to s3://android.mapzen.com/sample-snapshots/.
-
-./gradlew assembleDebug -PapiKey=$API_KEY
-s3cmd put sample/build/outputs/apk/mapzen-android-sdk-sample-debug.apk s3://android.mapzen.com/mapzen-android-sdk-sample-latest.apk
-s3cmd put sample/build/outputs/apk/mapzen-android-sdk-sample-debug.apk s3://android.mapzen.com/sample-snapshots/mapzen-android-sdk-sample-$CIRCLE_BUILD_NUM.apk
