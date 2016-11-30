7a3aabd22b8ac536a91e0935772939f3f54ec544
Sarah Lensing <sarah@mapzen.com>
2016-11-30 16:11:40 -0700

    Update deploy paths for new sample app locations

--- scripts/deploy-android-sdk-sample-app.sh
+++ scripts/deploy-android-sdk-sample-app.sh
@@ -3,5 +3,5 @@
 # Builds mapzen android sdk sample app and uploads APK to s3://android.mapzen.com/mapzen-android-sdk-sample-snapshots/.
 
 ./gradlew assembleDebug -PapiKey=$API_KEY
-s3cmd put mapzen-android-sdk-sample/build/outputs/apk/mapzen-android-sdk-sample-debug.apk s3://android.mapzen.com/mapzen-android-sdk-sample-latest.apk
-s3cmd put mapzen-android-sdk-sample/build/outputs/apk/mapzen-android-sdk-sample-debug.apk s3://android.mapzen.com/sample-snapshots/mapzen-android-sdk-sample-$CIRCLE_BUILD_NUM.apk
+s3cmd put samples/mapzen-android-sdk-sample/build/outputs/apk/samples_mapzen-android-sdk-sample-debug.apk s3://android.mapzen.com/mapzen-android-sdk-sample-latest.apk
+s3cmd put samples/mapzen-android-sdk-sample/build/outputs/apk/samples_mapzen-android-sdk-sample-debug.apk s3://android.mapzen.com/mapzen-android-sdk-sample-snapshots/mapzen-android-sdk-sample-$CIRCLE_BUILD_NUM.apk
--- scripts/deploy-places-api-sample-app.sh
+++ scripts/deploy-places-api-sample-app.sh
@@ -3,5 +3,5 @@
 # Builds mapzen places api sample app and uploads APK to s3://android.mapzen.com/mapzen-places-api-sample-snapshots/.
 
 ./gradlew assembleDebug -PapiKey=$API_KEY
-s3cmd put mapzen-places-api-sample/build/outputs/apk/mapzen-places-api-sample-debug.apk s3://android.mapzen.com/mapzen-places-api-sample-latest.apk
-s3cmd put mapzen-places-api-sample/build/outputs/apk/mapzen-places-api-sample-debug.apk s3://android.mapzen.com/places-api-sample-snapshots/mapzen-places-api-sample-$CIRCLE_BUILD_NUM.apk
+s3cmd put samples/mapzen-places-api-sample/build/outputs/apk/samples_mapzen-places-api-sample-debug.apk s3://android.mapzen.com/mapzen-places-api-sample-latest.apk
+s3cmd put samples/mapzen-places-api-sample/build/outputs/apk/samples_mapzen-places-api-sample-debug.apk s3://android.mapzen.com/places-api-sample-snapshots/mapzen-places-api-sample-$CIRCLE_BUILD_NUM.apk
