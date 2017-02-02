935aaab61bf07a7c55b39d721335effeb699025a
Chuck Greb <charles.greb@gmail.com>
2017-02-02 11:50:40 -0500

    Update deploy scripts to inject API key via build props

--- scripts/deploy-places-api-sample-app.sh
+++ scripts/deploy-places-api-sample-app.sh
@@ -2,6 +2,6 @@
 #
 # Builds mapzen places api sample app and uploads APK to s3://android.mapzen.com/mapzen-places-api-sample-snapshots/.
 
-./gradlew assembleDebug -PapiKey=$API_KEY
+./gradlew assembleDebug -PmapzenApiKey=$MAPZEN_API_KEY
 s3cmd put samples/mapzen-places-api-sample/build/outputs/apk/samples_mapzen-places-api-sample-debug.apk s3://android.mapzen.com/mapzen-places-api-sample-latest.apk
 s3cmd put samples/mapzen-places-api-sample/build/outputs/apk/samples_mapzen-places-api-sample-debug.apk s3://android.mapzen.com/places-api-sample-snapshots/mapzen-places-api-sample-$CIRCLE_BUILD_NUM.apk
