521ecacd7087bbf7603729e1eeca35688ee2b785
Chuck Greb <charles.greb@gmail.com>
2017-02-08 12:33:01 -0500

    Deploy sample apps to AWS on release build only

--- scripts/deploy-android-sdk-sample-app.sh
+++ scripts/deploy-android-sdk-sample-app.sh
@@ -2,6 +2,6 @@
 #
 # Builds mapzen android sdk sample app and uploads APK to s3://android.mapzen.com/mapzen-android-sdk-sample-snapshots/.
 
-./gradlew assembleDebug -PmapzenApiKey=$MAPZEN_API_KEY
+./gradlew :samples_mapzen-android-sdk-sample:assembleDebug -PmapzenApiKey=$MAPZEN_API_KEY
 s3cmd put samples/mapzen-android-sdk-sample/build/outputs/apk/samples_mapzen-android-sdk-sample-debug.apk s3://android.mapzen.com/mapzen-android-sdk-sample-latest.apk
 s3cmd put samples/mapzen-android-sdk-sample/build/outputs/apk/samples_mapzen-android-sdk-sample-debug.apk s3://android.mapzen.com/mapzen-android-sdk-sample-snapshots/mapzen-android-sdk-sample-$CIRCLE_BUILD_NUM.apk
--- scripts/deploy-places-api-sample-app.sh
+++ scripts/deploy-places-api-sample-app.sh
@@ -2,6 +2,6 @@
 #
 # Builds mapzen places api sample app and uploads APK to s3://android.mapzen.com/mapzen-places-api-sample-snapshots/.
 
-./gradlew assembleDebug -PmapzenApiKey=$MAPZEN_API_KEY
+./gradlew :samples_mapzen-place-api-sample:assembleDebug -PmapzenApiKey=$MAPZEN_API_KEY
 s3cmd put samples/mapzen-places-api-sample/build/outputs/apk/samples_mapzen-places-api-sample-debug.apk s3://android.mapzen.com/mapzen-places-api-sample-latest.apk
 s3cmd put samples/mapzen-places-api-sample/build/outputs/apk/samples_mapzen-places-api-sample-debug.apk s3://android.mapzen.com/places-api-sample-snapshots/mapzen-places-api-sample-$CIRCLE_BUILD_NUM.apk
