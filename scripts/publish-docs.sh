01a9bc396fa36a051ebbccddd80be8ee0a3a690c
Chuck Greb <ecgreb@users.noreply.github.com>
2017-02-14 15:41:13 -0500

    Deploy release directly from Circle CI (#304)
    * Deploy release directly from Circle CI
    
    * Remove `uploadArchives` task from release plugin config
    
    * Update release-checklist.md
    
    * Remove `uploadArchives` from core module too

--- scripts/publish-docs.sh
+++ scripts/publish-docs.sh
@@ -2,8 +2,5 @@
 #
 # Triggers mapzen docs build to publish to https://mapzen.com/documentation/.
 
-if [[ ${PERFORM_RELEASE} ]]
-  then
-    pip install 'Circle-Tickler == 1.0.1'
-    tickle-circle mapzen documentation master $CIRCLE_TOKEN
-fi
+pip install 'Circle-Tickler == 1.0.1'
+tickle-circle mapzen documentation master $CIRCLE_TOKEN
