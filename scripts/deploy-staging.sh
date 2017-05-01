d14332de6459a3c4154569e5f1015de1f87fc3cf
Chuck Greb <ecgreb@users.noreply.github.com>
2017-05-01 16:05:10 -0400

    Update deploy-staging.sh
    Update Circle CI environment vars

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -5,8 +5,8 @@
 
 echo -e "machine github.com\n  login $GITHUB_USERNAME\n  password $GITHUB_PASSWORD" >> ~/.netrc
 git clone https://github.com/mapzen/android-config.git
-./gradlew uploadArchives -PsonatypeUsername="$SONATYPE_USERNAME" \
-    -PsonatypePassword="$SONATYPE_PASSWORD" \
+./gradlew uploadArchives -PsonatypeUsername="$SONATYPE_NEXUS_SNAPSHOTS_USERNAME" \
+    -PsonatypePassword="$SONATYPE_NEXUS_SNAPSHOTS_PASSWORD" \
     -Psigning.keyId="$SIGNING_KEY_ID" \
     -Psigning.password="$SIGNING_PASSWORD" \
     -Psigning.secretKeyRingFile="$SIGNING_SECRET_KEY_RING_FILE"
