6d210a2903469b2ebb8b9201b85c0a8499843eb6
Sarah Lensing <sarah@mapzen.com>
2017-05-02 14:12:50 -0600

    deploy snapshot only if version contains SNAPSHOT

--- scripts/deploy-snapshot.sh
+++ scripts/deploy-snapshot.sh
@@ -3,5 +3,11 @@
 # Builds and uploads snapshot AARs to https://oss.sonatype.org/content/repositories/snapshots/com/mapzen/.
 #
 
-./gradlew uploadArchives -PsonatypeUsername=$SONATYPE_NEXUS_SNAPSHOTS_USERNAME  \
-    -PsonatypePassword=$SONATYPE_NEXUS_SNAPSHOTS_PASSWORD
+while read -r line || [[ -n "$line" ]]; do
+  if [[ $line =~ .*version=.*-SNAPSHOT.* ]]
+  then
+    ./gradlew uploadArchives -PsonatypeUsername=$SONATYPE_NEXUS_SNAPSHOTS_USERNAME  \
+        -PsonatypePassword=$SONATYPE_NEXUS_SNAPSHOTS_PASSWORD
+    break
+  fi
+done < "gradle.properties"
