e4085cb9788076855ada1c27dd2a0a3355e7a79b
Mapzen Developer <accounts+mapnerd@mapzen.com>
2017-05-02 17:21:53 -0600

    fix release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,13 +3,13 @@
 # runs the gradle release plugin
 #
 
-if ([ -n ${RELEASE_VERSION_NUMBER} ] && [ -n ${NEW_VERSION_NUMBER} ]); then
+if ([ -z $RELEASE_VERSION_NUMBER ] || [ -z $NEW_VERSION_NUMBER ]); then
+  echo "RELEASE_VERSION_NUMBER or NEW_VERSION_NUMBER not specified, skipping release."
+else
   echo "Releasing"
   git config --global user.email "accounts+mapnerd@mapzen.com"
   git config --global user.name "Mapzen Developer"
   ./gradlew release -Prelease.useAutomaticVersion=true \
     -Prelease.releaseVersion=$RELEASE_VERSION_NUMBER \
     -Prelease.newVersion=$NEW_VERSION_NUMBER
-else
-  echo "RELEASE_VERSION_NUMBER & NEW_VERSION_NUMBER not specified, skipping release."
 fi
