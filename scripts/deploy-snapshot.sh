bacd3b0d019d26e23410f235b04a90ef1b053e84
Sarah Lensing <sarah@mapzen.com>
2017-05-02 11:28:55 -0600

    add snapshot link to script

--- scripts/deploy-snapshot.sh
+++ scripts/deploy-snapshot.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 #
-# Builds and uploads snapshot AARs to https://oss.sonatype.org/#stagingRepositories.
+# Builds and uploads snapshot AARs to https://oss.sonatype.org/content/repositories/snapshots/com/mapzen/.
 #
 
 ./gradlew uploadArchives -PsonatypeUsername=$SONATYPE_NEXUS_SNAPSHOTS_USERNAME  \
     -PsonatypePassword=$SONATYPE_NEXUS_SNAPSHOTS_PASSWORD
-    
