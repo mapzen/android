6c1a9637a1ab4145344680d9ed89c13845c247f7
Sarah Lensing <sarahlensing@gmail.com>
2017-06-07 15:58:12 -0400

    Update feature pick listener docs (#401)
    * update feature pick listener docs
    
    * update doc
    
    * add marker interface docs

--- core/src/main/java/com/mapzen/android/graphics/MarkerPickListener.java
+++ core/src/main/java/com/mapzen/android/graphics/MarkerPickListener.java
@@ -3,7 +3,11 @@ package com.mapzen.android.graphics;
 import com.mapzen.android.graphics.model.BitmapMarker;
 
 /**
- * Listener invoked when a marker on the map is selected.
+ * Listener invoked when a marker on the map is selected. Note that all
+ * {@link com.mapzen.android.graphics.model.Polygon}s,
+ * {@link com.mapzen.android.graphics.model.Polyline}s, and
+ * search/routing pins are not markers and will receive pick events via either the
+ * {@link LabelPickListener} or {@link FeaturePickListener} interfaces.
  */
 public interface MarkerPickListener {
   /**
