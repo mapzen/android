0bddb1c2891cc6c4bd5f9cf1ed6610afbd5c136c
Sarah Lensing <sarahlensing@gmail.com>
2017-09-15 17:49:07 -0600

    Add new sample module (#453)
    * Add new sample app module
    
    * update sdk and build tools for modules and circle

---
+++ samples/mapzen-sample/src/main/java/mapzen/com/sdksampleapp/MainActivity.java
@@ -0,0 +1,12 @@
+package mapzen.com.sdksampleapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
