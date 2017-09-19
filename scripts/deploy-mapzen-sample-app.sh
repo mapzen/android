5b696855e76bef183b437b7346bbbd92380d6337
Sarah Lensing <sarahlensing@gmail.com>
2017-09-19 10:56:41 -0600

    fix apk path

--- scripts/deploy-mapzen-sample-app.sh
+++ scripts/deploy-mapzen-sample-app.sh
@@ -3,5 +3,5 @@
 # Builds mapzen sdk sample app and uploads APK to s3://android.mapzen.com/mapzen-sdk-sample-snapshots/.
 
 ./gradlew :samples_mapzen-sample:assembleDebug -PmapzenApiKey=$MAPZEN_API_KEY
-s3cmd put samples/mapzen-sdk-sample/build/outputs/apk/samples_mapzen-sdk-sample-debug.apk s3://android.mapzen.com/mapzen-sdk-sample-latest.apk
-s3cmd put samples/mapzen-sdk-sample/build/outputs/apk/samples_mapzen-sdk-sample-debug.apk s3://android.mapzen.com/mapzen-sdk-sample-releases/mapzen-sdk-sample-$CIRCLE_TAG.apk
+s3cmd put samples/mapzen-sample/build/outputs/apk/samples_mapzen-sample-debug.apk s3://android.mapzen.com/mapzen-sdk-sample-latest.apk
+s3cmd put samples/mapzen-sample/build/outputs/apk/samples_mapzen-sample-debug.apk s3://android.mapzen.com/mapzen-sdk-sample-releases/mapzen-sdk-sample-$CIRCLE_TAG.apk
