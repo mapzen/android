7280876e3e876d5ec9fb918984de335a88aac754
Sarah Lensing <sarahlensing@gmail.com>
2017-10-03 13:33:20 -0600

    check map has been created before resetting listener

--- samples/mapzen-android-sdk-sample/src/main/java/com/mapzen/android/sdk/sample/BasicMapzenActivity.java
+++ samples/mapzen-android-sdk-sample/src/main/java/com/mapzen/android/sdk/sample/BasicMapzenActivity.java
@@ -89,7 +89,9 @@ public class BasicMapzenActivity extends BaseDemoActivity
 
   @Override protected void onDestroy() {
     super.onDestroy();
-    map.setFindMeOnClickListener(null);
+    if (map != null) {
+      map.setFindMeOnClickListener(null);
+    }
   }
 
   @Override public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
