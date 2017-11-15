eea8ccd7cb7674349b9d7afcccf895db7490ba14
Sarah Lensing <sarahlensing@gmail.com>
2017-11-15 15:02:04 -0700

    update apk path and add snapshot deployment script for master builds

--- scripts/deploy-mapzen-sample-app-snapshot.sh
+++ scripts/deploy-mapzen-sample-app-snapshot.sh
@@ -4,4 +4,4 @@
 
 ./gradlew :samples_mapzen-sample:assembleDebug -PmapzenApiKey=$MAPZEN_API_KEY
 s3cmd put samples/mapzen-sample/build/outputs/apk/debug/samples_mapzen-sample-debug.apk s3://android.mapzen.com/mapzen-sdk-sample-latest.apk
-s3cmd put samples/mapzen-sample/build/outputs/apk/debug/samples_mapzen-sample-debug.apk s3://android.mapzen.com/mapzen-sdk-sample-releases/mapzen-sdk-sample-$CIRCLE_TAG.apk
+s3cmd put samples/mapzen-sample/build/outputs/apk/debug/samples_mapzen-sample-debug.apk s3://android.mapzen.com/mapzen-sdk-sample-snapshots/mapzen-sdk-sample-$CIRCLE_BUILD_NUM.apk
